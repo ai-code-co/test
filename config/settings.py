8719d6d0b7c6b40d97b699cdb99aa51a162f7b84
pycode <devpython549@gmail.com>
2025-12-30 20:34:22 +0530

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -86,17 +86,17 @@ DATABASES = {
 }
 
 
-# -------------------- Database --------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
-    }
-}
+# # -------------------- Database --------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST", "localhost"),
+#         'PORT': os.getenv("DB_PORT", "3306"),
+#     }
+# }
 
 
 # Update database from DATABASE_URL
