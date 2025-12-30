0be41fb6f781c028c2e75ae889f065b94577ff68
pycode <devpython549@gmail.com>
2025-12-30 20:17:04 +0530

    Initial commit

---
+++ user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    new_number = models.CharField()
+
+    def __str__(self):
+        return self.name
